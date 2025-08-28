29df3162bb405e48965aabda02bf8572cbd7e3d3
Aoumeur141 <aoumeurmohamed2404@gmail.com>
2025-08-28 09:50:52 +0100

    Update __init__.py

--- src/pytaps/__init__.py
+++ src/pytaps/__init__.py
@@ -1,5 +1,38 @@
 
 
+"""
+pytap : Python Toolkit for Algerian Production suite
+
+Copyright (c) 2025, Météo Algérie
+------------------------------------------------------------------
+
+``pytap`` is a package of classes and functions, designed to handle daily tasks
+related to meteorological data processing and analysis, developed by Météo-Algérie.
+
+It provides utilities for:
+- Data fetching and configuration.
+- General utility functions.
+- Specific data manipulation and processing (e.g., GRIB files).
+- Email communication.
+- Logging and system interactions.
+- Numerical operations (NumPy).
+- Date and time handling.
+- File system operations.
+- 
+
+********************************************************************************
+"""
+
+# --- Package Metadata ---
+__version__ = "1.0.0"
+__license__ = 'CeCILL-C'
+
+
+# --- Custom Package Exception ---
+class PyTapError(Exception):
+    """Base exception class for the pytap package."""
+    pass
+
 from . import data_utils
 from . import email_utils
 from . import fetchdata
@@ -9,6 +42,8 @@ from . import numpy_utils
 from . import system_utils
 from . import date_time_utils
 from . import file_operations
+from . import config_utils
+
 
 __version__ = "0.1.0"
 __all__ = [
@@ -21,4 +56,5 @@ __all__ = [
     "system_utils",
     "date_time_utils",
     "file_operations",
+    "config_utils",
 ]
